b1e4b685f2b9a0081420b051c9138b57bd993180
Sarthak Mishra <56118819+sarthakmishraa@users.noreply.github.com>
2024-02-13 18:16:09 -0500

    Add files via upload

---
+++ src/components/Instructions.tsx
@@ -0,0 +1,29 @@
+// Instructions.tsx
+
+const Instructions = () => {
+  return(
+    <div className="Instructions">
+      <h2>Drag and drop tasks to move them between "Tasks Left" and "Tasks Done" sections.</h2>
+
+      {/* <ul>Adding Tasks:
+        <li>Enter the task details in the input field and press "Enter" button.</li>
+      </ul>
+
+      <ul>Editing Tasks:
+        <li>Click on the edit icon given on the task you want to edit.</li>
+        <li>Enter the task details in the input field and press "Enter".</li>
+      </ul>
+
+      <ul>Removing Tasks:
+        <li>Click on the delete(trash) icon to remove the task.</li>
+      </ul>
+
+      <ul>Moving Tasks:
+        <li>To mark a task as done, drag and drop it from the "Tasks Left" section to the "Tasks Done" section.</li>
+        <li>To undo, drag it back from "Tasks Done" to "Tasks Left".</li>
+      </ul> */}
+    </div>
+  )
+}
+
+export default Instructions;
